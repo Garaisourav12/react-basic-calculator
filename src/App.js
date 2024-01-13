187ffa6a7a21fc979c22b6d2d85e3a0c0c57f9af
Garaisourav12 <garaisourav12@gmail.com>
2024-01-13 20:03:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [num1, setNum1] = useState('');
+    const [num2, setNum2] = useState('');
+
+    const [res, setRes] = useState(null);
+    const [msg, setMsg] = useState(null);
+
+    const calculate = (result) => {
+        if(!num1) {
+            setRes('Num1 Cannot Be Empty')
+            setMsg('Error')
+        }
+        else if(!num2) {
+            setRes('Num2 Cannot Be Empty')
+            setMsg('Error')
+        }
+        else if(isNaN(Number(num1))){
+            setRes('Num1 Should Be Either Integer, Floating-Point Number, Positive, Or Negative')
+            setMsg('Error')
+        }
+        else if(isNaN(Number(num2))){
+            setRes('Num2 Should Be Either Integer, Floating-Point Number, Positive, Or Negative')
+            setMsg('Error')
+        }
+        else{
+            setRes('Result : ' + result);
+            setMsg('Success')
+        }
+    }
+
+    return (
+        <div className='app'>
+            <div className="main">
+                <h1>React Calculator</h1>
+                <div className="calculator">
+                    <input type="text" placeholder='Num 1' value={num1} onChange={(e) => {setRes(null); setNum1(e.target.value)}} />
+                    <input type="text" placeholder='Num 2' value={num2} onChange={(e) => {setRes(null); setNum2(e.target.value)}} />
+                    <div className="btns">
+                        <button onClick={() => calculate(Number(num1) + Number(num2))}>+</button>
+                        <button onClick={() => calculate(Number(num1) - Number(num2))}>-</button>
+                        <button onClick={() => calculate(Number(num1) * Number(num2))}>*</button>
+                        <button onClick={() => calculate(Number(num1) / Number(num2))}>/</button>
+                    </div>
+                </div>
+
+                {
+                    res && (
+                        <>
+                            <div className={`msg ${msg}`}>{msg}!</div>
+                            <div className="result">{res}</div>
+                        </>
+                    )
+                }
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
